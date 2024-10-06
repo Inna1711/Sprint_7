4f2528b85b6184b797c3e8ce0bf9eba4a168397c
Inna Besedowskaya <innessa.besedowskaya@yandex.ru>
2024-10-07 02:30:29 +0300

    Added orders

---
+++ src/test/java/models/OrderCreate/Output.java
@@ -0,0 +1,13 @@
+package models.OrderCreate;
+
+public class Output {
+    private int track;
+
+    public int getTrack() {
+        return track;
+    }
+
+    public void setTrack(int track) {
+        this.track = track;
+    }
+}
